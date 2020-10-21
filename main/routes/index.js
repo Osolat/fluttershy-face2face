1e344cd5fca4a085aa31431257f039e57432f457
Benjamin Birch Hansen <bbhbenjamin@gmail.com>
2020-10-21 12:08:25 +0200

    local db

--- main/routes/index.js
+++ main/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-
+const fs = require('fs');
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.sendFile(path.join(__dirname + '/../public/frontPage.html'));
@@ -11,6 +11,17 @@ function addGroupToDataBase(id, password) {
     console.log("Group ID: " + id);
     console.log("Password: " + password);
 
+    fs.readFile('./database.json', (err, data) => {
+        if (err) throw err;
+        let groupData = JSON.parse(data);
+        groupData[id] = {password: password}
+
+        fs.writeFile('./database.json', JSON.stringify(groupData), (err) => {
+            if (err) throw err;
+            console.log('New group data written to local json database');
+            console.log(groupData);
+        });
+    });
 }
 
 router.post('/', express.json({type: '*/*'}), (req, res) => {
